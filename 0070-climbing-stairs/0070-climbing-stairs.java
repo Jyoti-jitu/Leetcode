2b6dcfd489b42ab743ee03cd84bfa66fc168fac3
Jyoti-jitu <parhijyotiswarup@gmail.com>
2026-08-15 00:14:22 +0530

    Time: 0 ms (100%), Space: 42.1 MB (55.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n== 1 || n==2) return n ;
-        int prev1=2;
-        int prev2=1;
-        int result =0 ;
-        for(int i = 3; i <= n ; i++){
-            result = prev1+prev2;
-            prev2= prev1;
-            prev1 = result;
+        if(n<=2){
+            return n;
         }
-        return result;
+        int prev1 = 2;
+        int prev2 = 1;
+    for(int i=3;i<=n;i++){
+     int current = prev1 + prev2;
+
+     prev2 = prev1;
+     prev1 = current;
+    }
+    return prev1;
     }
 }
