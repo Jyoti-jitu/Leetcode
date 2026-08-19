9ba77cd34f8c9cd8679746a2f5fb75cad2cd27f9
Jyoti-jitu <parhijyotiswarup@gmail.com>
2026-08-19 12:03:41 +0530

    Time: 461 ms (45.45%), Space: 0B (100%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,11 @@
+-- # Write your MySQL query statement below
+-- SELECT author_id AS id
+-- FROM Views 
+-- GROUP BY author_id , viewer_id
+-- HAVING  author_id = viewer_id
+-- ORDER BY author_id ASC;
+
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC;
